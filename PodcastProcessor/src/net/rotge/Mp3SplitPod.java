deca5ee71a9626dc58bf2b58173c9ef0a911fe03
Roger Rotge <roger.rotge@g.austincc.edu>
2015-12-04 06:12:07 -0600

    added a few comments and strings to build the command as well as some
    output to the console to let the user know what's going on

--- PodcastProcessor/src/net/rotge/Mp3SplitPod.java
+++ PodcastProcessor/src/net/rotge/Mp3SplitPod.java
@@ -12,38 +12,43 @@ import java.nio.file.Paths;
 public class Mp3SplitPod {
 
 	final static long sizeOfMedium = 36700160;
-	private static final String SPLITTIME = "14.0";
+	private static final String SPLITTIME = " -t 14.0";
+	private static String splitCommand = "/usr/bin/mp3splt";
+	private static String outDirParameter = "-d";
 	private Path currentWorkingDirectory;
 
 	public void split(File incFile, Path incDir) throws IOException {
 		this.currentWorkingDirectory = incDir.toAbsolutePath();
 
-		String[] mp3spltCommand = { "mp3splt", "-t", SPLITTIME, "-d", currentWorkingDirectory.toString(),
+		String[] mp3spltCommand = { splitCommand, SPLITTIME, outDirParameter, currentWorkingDirectory.toString(),
 				incFile.toString() };
 
 		ProcessBuilder probuilder = new ProcessBuilder(mp3spltCommand);
-		probuilder.directory(new File("/usr/bin"));
-
 		Process process = probuilder.start();
 
 		InputStream is = process.getErrorStream();
 		InputStreamReader isr = new InputStreamReader(is);
 		BufferedReader br = new BufferedReader(isr);
 		String line;
+		System.out.println("Splitting file: " + incFile.toString());
 		while ((line = br.readLine()) != null) {
+			// uncomment for console output
 			// System.out.println(line);
 		}
-
 		try {
 			int exitValue = process.waitFor();
-			System.out.println("ExitValue is " + exitValue);
+			if (exitValue == 0) {
+				System.out.println("Success!");
+			} else {
+				System.out.println("ExitValue is " + exitValue);
+			}
 		} catch (InterruptedException e1) {
 			e1.printStackTrace();
 		}
 		cleanUpSource(incFile.toPath());
-
 	}
 
+	// This sub-method deletes the original file that was split
 	private void cleanUpSource(Path fileToDelete) {
 		try {
 			System.out.println("Deleting: " + Paths.get(fileToDelete.toString()));
@@ -51,17 +56,6 @@ public class Mp3SplitPod {
 		} catch (IOException e) {
 			System.out.println("Problem deleting source file in MP3SPLT");
 			e.printStackTrace();
-
 		}
 	}
-
 }
-
-// Path path = Paths.get("data/subdir/logging-moved.properties");
-//
-// try {
-// Files.delete(path);
-// } catch (IOException e) {
-// //deleting file failed
-// e.printStackTrace();
-// }
